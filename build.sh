40a9a303e40caf501db037c8ab73fef2fb68c5d8
Alexey Simonov <alexey.simonov@gmail.com>
2026-04-22 15:17:58 +0400

    SpacecraftAI image tag and push commands

--- build.sh
+++ build.sh
@@ -7,3 +7,19 @@
 earthly +main-image
 
 earthly +dev-image
+
+# tag and push the image to Docker Hub
+export OS=`uname`
+export ARCH=unknown
+if [[ "$OS" =~ "Darwin" ]]; then
+  export ARCH=`uname -m` # should be arm64 on apple silicon
+elif [[ "$OS" =~ "Linux" ]]; then
+  export ARCH=`dpkg --print-architecture` # should be amd64 on Ubuntu for x86_64
+else
+  echo "cannot determine operating system/architecture"
+  exit 1
+fi
+
+export SPACEROS_TAG=2026.01
+docker tag tiipsrc/space-ros:latest tiipsrc/space-ros:scai-$ARCH-$SPACEROS_TAG
+docker push tiipsrc/space-ros:scai-$ARCH-$SPACEROS_TAG
